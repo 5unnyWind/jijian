403f2e3499d23a5687e6612ad2ab2540c1084765
tingfeng <sunny-wind@foxmail.com>
2024-05-15 01:42:40 +0800

    fix: build

--- app/api/sql/route.ts
+++ app/api/sql/route.ts
@@ -2,11 +2,11 @@ import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const someSql = async () => sql`
-  ALTER TABLE users
-  ADD CONSTRAINT unique_username UNIQUE (username);  
-  `;
+  // const someSql = async () => sql`
+  // ALTER TABLE users
+  // ADD CONSTRAINT unique_username UNIQUE (username);  
+  // `;
 
-  await someSql();
+  // await someSql();
   return NextResponse.json({ message: "done" }, { status: 200 });
 }
