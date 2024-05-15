fdcd90ff9be56c0e8380f0f5e32025495daf3230
tingfeng <sunny-wind@foxmail.com>
2024-05-15 15:11:35 +0800

    fix: log error

--- app/lib/session.ts
+++ app/lib/session.ts
@@ -21,12 +21,14 @@ export async function encrypt(payload: SessionPayload) {
 
 export async function decrypt(session: string | undefined = "") {
   try {
+    console.log("secretKey", secretKey);
+    console.log("encodedKey", encodedKey);
     const { payload } = await jwtVerify(session, encodedKey, {
       algorithms: ["HS256"],
     });
     return payload;
   } catch (error) {
-    console.log("Failed to verify session");
+    console.log("Failed to verify session", error);
   }
 }
 
