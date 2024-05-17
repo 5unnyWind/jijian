93cc02568143819617e40c5eecb58d661df88aec
tingfeng <sunny-wind@foxmail.com>
2024-05-17 23:09:57 +0800

    feat: Skeleton

--- app/home/calendar/page.tsx
+++ app/home/calendar/page.tsx
@@ -1,8 +1,9 @@
 import { Button } from "@/app/lib/Button";
 import Back from "../Back";
 import { Calendar as Calendar_cmp } from "@/app/lib/Calendar";
-import React from "react";
+import React, { Suspense } from "react";
 import { cookies } from "next/headers";
+import { Skeleton } from "@/app/lib/Skeleton";
 
 const HOST = process.env.NEXT_PUBLIC_HOST || "";
 
@@ -15,23 +16,30 @@ const getDisposedDates: () => Promise<{
     },
   }).then((res) => res.json());
 
-export default async function Calendar() {
-  const data = await getDisposedDates();
-  const disposedDates = data.disposedDates.map(
-    (date) => new Date(date.disposed_at)
-  );
-
+export default function Calendar() {
   return (
     <main className="w-full">
       <Back />
       <div className="flex flex-col items-center">
-        <Calendar_cmp
-          mode="multiple"
-          selected={disposedDates}
-          // onSelect={setDate}
-          className="rounded-md border mt-10"
-        />
+        <Suspense fallback={<Skeleton className="w-[224px] h-[261px]" />}>
+          <CalendarWrapper />
+        </Suspense>
       </div>
     </main>
   );
 }
+
+const CalendarWrapper = async () => {
+  const data = await getDisposedDates();
+  const disposedDates = data.disposedDates.map(
+    (date) => new Date(date.disposed_at)
+  );
+  return (
+    <Calendar_cmp
+      mode="multiple"
+      selected={disposedDates}
+      // onSelect={setDate}
+      className="rounded-md border mt-10"
+    />
+  );
+};
