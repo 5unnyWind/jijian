b8a44a9d95f59718e141187423aa2d9da66d60c1
tingfeng <sunny-wind@foxmail.com>
2024-05-18 13:33:11 +0800

    feat: cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // reactStrictMode:false
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://jijian.tingfeng.blue", // Set your origin
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
